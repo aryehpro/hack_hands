a5aed3a153f775ff69d09f4dd039404de2b9e080
aryehpro <aryehpro@gmail.com>
2015-09-25 23:19:22 +0300

    implemented config structure + defined user model in mongoose + implemented user addition via post requests + installed body-parser module

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-var port = 2020;
+var config = require('./config/config');
+var mongoose = require('./config/mongoose');
 var express = require('./config/express');
+
+var db = mongoose();
 var app = express();
 
-app.listen(port);
+app.listen(config.port);
 
 module.exports = app;
 
-console.log('Server Running at http://localhost:' + port);
+console.log(process.env.NODE_ENV + ' server running at http://localhost:' + config.port);
+console.log('mongoose version: ' + db.version);
