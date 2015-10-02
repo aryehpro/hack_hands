088d5c1c1099c77f5c7f50e5f7c05b0f6ab047be
aryehpro <aryehpro@gmail.com>
2015-10-02 21:06:14 +0300

    updated some of the node modules, added passport modules, added passport and local strategy config, and udated user model to support authentication.

---
+++ config/passport.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Aryeh on 30/09/2015.
+ */
+var passport = require('passport');
+var mongoose = require('mongoose');
+
+module.exports = function() {
+    var User = mongoose.model('User');
+
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        User.findOne(
+            {_id: id},
+            '-password',
+            function(err, user){
+                done(err, user);
+            }
+        );
+    });
+
+    require('./strategies/local')();
+}
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var config = require('./config/config');
 var mongoose = require('./config/mongoose');
 var express = require('./config/express');
+var passport = require('./config/passport');
 
 var db = mongoose();
 var app = express();
+var passport = passport();
 
 app.listen(config.port);
 
